09b4ab2730a1753d2a3ad897faeaa5df937f8ece
Gabriel <43073074+Gabixel@users.noreply.github.com>
2026-06-07 18:27:49 +0000

    Add service worker logic (not working on dev mode) + edit README

---
+++ my-app/src/service-worker.ts
@@ -0,0 +1,42 @@
+// Disables access to DOM typings like `HTMLElement` which are not available
+// inside a service worker and instantiates the correct globals
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+// Ensures that the `$service-worker` import has proper type definitions
+/// <reference types="@sveltejs/kit" />
+
+import { build, files, version } from "$service-worker";
+
+// Giving `self` the correct types
+const self = globalThis.self as unknown as ServiceWorkerGlobalScope;
+
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+	...build, // the app itself
+	...files, // everything in `static`
+];
+
+self.addEventListener("install", (event) => {
+	// Create a new cache and add all files to it
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener("activate", (event) => {
+	// Remove previous cached data from disk
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) await caches.delete(key);
+		}
+	}
+
+	event.waitUntil(deleteOldCaches());
+});
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,15 +1,17 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { type UserConfig } from "vite";
 
-import sveltekitConfig from "./svelte.config.js";
+// import sveltekitConfig from "./svelte.config.js";
 
 const config = {
 	plugins: [
-		sveltekit({
-			...(sveltekitConfig.kit as any),
-			compilerOptions: sveltekitConfig.compilerOptions as any,
-			vitePlugin: {},
-		}),
+		sveltekit(
+			// 	{
+			// 	// ...(sveltekitConfig.kit as any),
+			// 	// compilerOptions: sveltekitConfig.compilerOptions as any,
+			// 	// vitePlugin: {},
+			// }
+		),
 	],
 	define: {
 		"process.env.NODE_ENV":

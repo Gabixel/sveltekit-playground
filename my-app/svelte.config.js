03320563f53c5b586972b4ea2d297587f3419c74
Gabriel <43073074+Gabixel@users.noreply.github.com>
2026-06-07 17:03:18 +0000

    Create example app

---
+++ my-app/svelte.config.js
@@ -0,0 +1,44 @@
+// import * as child_process from "node:child_process";
+import adapter from "@sveltejs/adapter-vercel";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+		runes: /** @param {Object} o @param {string} o.filename */ ({ filename }) =>
+			filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+		// sourcemap: {},
+	},
+	kit: {
+		output: {},
+		csp: {
+			mode: "hash",
+		},
+		adapter: adapter(
+			/** @see https://svelte.dev/docs/kit/adapter-vercel */
+			// TODO: https://svelte.dev/docs/kit/single-page-apps#Usage
+			{
+				split: false,
+			},
+		),
+		paths: {},
+		serviceWorker: {
+			// TODO
+			register: false,
+		},
+		router: {
+			type: "hash",
+			resolution: "client",
+		},
+
+		// version: {
+		// 	name: child_process
+		// 		// TODO: check if "git" command exists?
+		// 		.execSync("git rev-parse HEAD")
+		// 		.toString()
+		// 		.trim(),
+		// },
+	},
+};
+
+export default config;
---
+++ my-app/vite.config.ts
@@ -0,0 +1,20 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { type UserConfig } from "vite";
+
+import sveltekitConfig from "./svelte.config.js";
+
+const config = {
+	plugins: [
+		sveltekit({
+			...(sveltekitConfig.kit as any),
+			compilerOptions: sveltekitConfig.compilerOptions as any,
+			vitePlugin: {},
+		}),
+	],
+	define: {
+		"process.env.NODE_ENV":
+			process.env.NODE_ENV === "production" ? '"production"' : '"development"',
+	},
+} satisfies UserConfig;
+
+export default config;
